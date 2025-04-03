f46d009a334d9da647bc380fea86175f02ec6ac9
alenachankunju <alenachankunju@gmail.com>
2025-04-03 15:54:56 +0530

    second commit

--- XCountry/src/Component/CountryCard.jsx
+++ XCountry/src/Component/CountryCard.jsx
@@ -1,5 +1,4 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const API_ENDPOINT = "https://xcountries-backend.azurewebsites.net/all";
 
@@ -18,7 +17,7 @@ const Card = ({ country }) => {
         borderRadius: "10px",
       }}
     >
-      <img width="100" src={country.flag} alt={`Flag of ${country.abbr}`} />
+      <img width="100" src={country.flag} alt={`Flag of ${country.name}`} />
       <p>{country.name}</p>
     </div>
   );
@@ -31,20 +30,26 @@ function CountryCard() {
     const fetchCountry = async () => {
       try {
         const response = await fetch(API_ENDPOINT);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
         const json = await response.json();
         setData(json);
       } catch (error) {
-        "Error fetching data: ", error;
+        console.error("Error fetching data:", error);
       }
     };
 
     fetchCountry();
   }, []);
+
   return (
     <>
-      {data.map((item, index) => (
-        <Card key={index} country={item} />
-      ))}
+      {data.length > 0 ? (
+        data.map((item, index) => <Card key={index} country={item} />)
+      ) : (
+        <p>Loading...</p>
+      )}
     </>
   );
 }
